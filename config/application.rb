b0777e2e7048c4682d857b39c4ce12381672208c
Farley Knight <farleyknight@gmail.com>
2013-03-05 18:03:56 -0500

    Add test for personal config

--- config/application.rb
+++ config/application.rb
@@ -62,5 +62,11 @@ module Ryte
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.after_initialize do
+      if Settings.personal.blank?
+        raise "Personal section from settings.yml! Please see the README."
+      end
+    end
   end
 end
